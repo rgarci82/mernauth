6d76e541fd03820a3503f29f5061968274aeece0
rgarci82 <ruben.garcia30102@gmail.com>
2023-10-27 20:53:07 -0500

    created routes and user model

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,45 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+
 //@desc  auth user/set token
 //route  POST /api/users/auth
 //access Pubic
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
-};
+});
 
-export { authUser };
+//@desc  Register a new user
+//route  POST /api/users
+//access Pubic
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+//@desc  Logout user
+//route  POST /api/users/logout
+//access Pubic
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+//@desc  Get user profile
+//route  GET /api/users/profile
+//access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+//@desc  Update user profile
+//route  PUT /api/users/profile
+//access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update user profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
